d13ce6631b48fed9532d313e085fecae7fcfa419
user <user@192.168.2.116>
2021-05-21 23:06:56 +0900

    chapter1 add

---
+++ java11silver/chapter1/src/example/Sample.java
@@ -0,0 +1,13 @@
+package example;
+
+import example.print.Print;
+
+public class Sample {
+
+	public static void main(String[] args) {
+
+		new Print().print(args[0]);
+
+	}
+
+}
---
+++ java11silver/chapter1/src/example/print/Print.java
@@ -0,0 +1,11 @@
+package example.print;
+
+public class Print {
+
+	public void print(String msg) {
+
+		System.out.println("print:" + msg);
+
+	}
+
+}
