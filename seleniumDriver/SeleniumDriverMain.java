7219a558b51adb7be128e2a75141287c440dce79
koukuma5968 <55043195+koukuma5968@users.noreply.github.com>
2023-07-08 12:36:07 +0900

    Add files via upload

---
+++ seleniumDriver/SeleniumDriverMain.java
@@ -0,0 +1,43 @@
+package seleniumDriver.main;
+
+import java.nio.file.Paths;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeDriverService;
+
+public class SeleniumDriverMain {
+
+    private static final String ROOT_DIR = Paths.get("").toAbsolutePath().toString();
+
+    public static void main(String[] args) {
+        setSystemProperty();
+        // webdriverを初期化
+        EdgeDriver driver = new EdgeDriver();
+        try {
+            // 対象URLをブラウザに表示
+            driver.get("https://www.google.co.jp/");
+            // 検索欄のweb要素を取得する
+            WebElement ele = driver.findElement(By.id("APjFqb"));
+            // 検索欄に「selenium」を入力
+            ele.sendKeys("selenium");
+            Thread.sleep(5000);
+            // 検索ボタンを押す
+            driver.findElement(By.name("btnK")).click();
+            Thread.sleep(5000);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            // ブラウザを閉じ、seleniumを終了させる。
+            driver.quit();
+        }
+    }
+
+    private static void setSystemProperty() {
+        // システムプロパティにedgeDriverのバスを登録します。
+        System.setProperty(EdgeDriverService.EDGE_DRIVER_EXE_PROPERTY,
+                ROOT_DIR  + "\\msedgedriver.exe");
+    }
+
+}
